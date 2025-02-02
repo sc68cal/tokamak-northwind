5a7cd173fed929f272e8dbfd5aa3aab729d97181
Sean M. Collins <sean@coreitpro.com>
2025-02-01 22:25:57 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Tokamak & Northwind practice
+
+This uses [zig][zig], the [tokamak][tk] library, [zqlite][zqlite] and
+a [sample database][northwind] to learn zig for use in
+web application development.
+
+[zig]: ziglang.org
+[tk]: https://github.com/cztomsik/tokamak
+[northwind]: https://github.com/jpwhite3/northwind-SQLite3
+[zqlite]: https://github.com/karlseguin/zqlite.zig
---
+++ src/main.zig
@@ -0,0 +1,41 @@
+const std = @import("std");
+const tk = @import("tokamak");
+const zqlite = @import("zqlite");
+
+const Region = struct {
+    RegionID: i64,
+    RegionDescription: []const u8,
+};
+
+const routes: []const tk.Route = &.{
+    .get("/Regions/:id", regionDetail),
+};
+
+fn regionDetail(res: *tk.Response, conn: zqlite.Conn, id: i64) !void {
+    if (try conn.row("select * from Regions where RegionID=?", .{id})) |row| {
+        defer row.deinit();
+        const result = Region{
+            .RegionID = row.int(0),
+            .RegionDescription = row.text(1),
+        };
+        try res.json(result, .{});
+    } else {
+        res.status = 404;
+        res.body = "Not Found";
+    }
+}
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+
+    const lopts: tk.ListenOptions = .{ .port = 8000 };
+
+    var conn = try zqlite.open("./northwind.db", zqlite.OpenFlags.EXResCode);
+    var cx = .{&conn};
+
+    const server = try tk.Server.init(gpa.allocator(), routes, .{ .listen = lopts, .injector = tk.Injector.init(&cx, null) });
+    defer server.deinit();
+
+    try server.start();
+}
